243c4d99b169019472b38112bff6a04529b70d6c
최진호 <jhchoi3669@gmail.com>
2025-11-15 18:12:19 +0900

    index.css 경로 수정

--- src/store/newsStore.js
+++ src/store/newsStore.js
@@ -3,9 +3,7 @@ import { create } from "zustand";
 export const useNewsStore = create((set) => ({
   newsGroup: [],
   selectedNewsGroup: [],
-  news: null,
 
   setNewsGroup: (newsGroup) => set({ newsGroup }),
   setSelectedNewsGroup: (selectedNewsGroup) => set({ selectedNewsGroup }),
-  setNews: (oneNews) => set({ oneNews }),
 }));
