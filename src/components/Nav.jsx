62d6d3cd211ca27f4601b608cd43c5fb4afbd73c
최진호 <jhchoi3669@gmail.com>
2025-11-21 16:20:37 +0900

    useLocation import 위치 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,4 @@
-import { useLocation } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useUiStore } from "../store/uiStore.js";
 import { useUserStore } from "../store/userStore.js";
 
